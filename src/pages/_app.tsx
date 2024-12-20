c730b12d89c3f48d5c7b1c0f9a94589607eddc20
Wagnetto <wagnetto@protonmail.com>
2024-12-20 14:48:42 -0300

    feature: Add Image component on Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import {AppProps} from 'next/app'
 import {globalStyles} from '../styles/global'
 import logoImg from '../assets/logo.png'
 import {Container, Header, LogoContainer} from '../styles/pages/app'
+import Image from 'next/image'
 
 globalStyles()
 
@@ -10,7 +11,7 @@ export default function App({Component, pageProps}: AppProps) {
     <Container>
       <Header>
         <LogoContainer>
-          <img src={logoImg.src} alt='logo' width='40px' />
+          <Image src={logoImg} alt='logo' width='40px' height='40px' />
           <h1>Next.js shop</h1>
         </LogoContainer>
       </Header>
