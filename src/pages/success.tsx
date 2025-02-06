33632a8d423dd88991ed511364abbc597e904ae3
Wagnetto <wagnetto@protonmail.com>
2025-02-06 17:52:35 -0300

    Add Success page structure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,18 @@
+import Link from 'next/link'
+import { ImageContainer, SuccessContainer } from '../styles/pages/success'
+
 export default function Success() {
-  return <h1>Success !</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra Efetuada!</h1>
+      <ImageContainer></ImageContainer>
+      <p>
+        Obaa! <span>[Nome do Cliente]</span>, sua compra{' '}
+        <span>[nome do produto]</span> foi efetuada e em breve estará a caminho
+        da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
